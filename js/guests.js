1550d02e10bb78fb0f4aea5a5f11e13bcc67f20a
Javier G. Aguilar <javig.aguilar12@gmail.com>
2025-08-27 15:12:56 -0400

    update
    update

--- js/guests.js
+++ js/guests.js
@@ -1,278 +1,302 @@
 const guestList = {
     "VzH3oKp": {
-        names: "JAVIER AGUILAR Y SRA ESPOSA",
+        names: "Javier Aguilar y Esposa",
         passes: 2
     },
     "kE6wR8g": {
-        names: "DANIER MELGAR Y SRA",
+        names: "Danier Melgar y Esposa",
         passes: 2
     },
     "pL2jA1d": {
-        names: "MIGUEL AGUILAR Y NOVIA",
+        names: "Miguel Aguilar y Novia",
         passes: 2
     },
     "bN9sY5f": {
-        names: "CHRISTIAN AGUILAR Y ESPOSA",
+        names: "Christian Aguilar y Esposa",
         passes: 3
     },
     "qG4cT7h": {
-        names: "NELLY AGUILAR",
+        names: "Nelly Aguilar",
         passes: 2
     },
     "xJ1vE9k": {
-        names: "ARIEL VARGAS",
+        names: "Ariel Vargas",
         passes: 1
     },
     "uM8zP3m": {
-        names: "YANIO VARGAS Y NOVIA",
+        names: "Yanio Vargas y Novia",
         passes: 2
     },
     "aI5oW6n": {
-        names: "LENY MELGAR",
+        names: "Leny Melgar",
         passes: 1
     },
     "fC2lB7r": {
-        names: "MELYHAN VARGAS",
+        names: "Melyhan Vargas",
         passes: 1
     },
     "sD9vX8t": {
-        names: "IVANA MELGAR",
+        names: "Ivana Melgar",
         passes: 1
     },
     "wZ6kY9u": {
-        names: "IBER MELGAR",
+        names: "Iber Melgar",
         passes: 1
     },
     "tA3jE4v": {
-        names: "CARLOS MELGAR Y SRA",
+        names: "Carlos Melgar y Sra",
         passes: 2
     },
     "hB7dN5w": {
-        names: "RUDDY MELGAR Y SRA",
+        names: "Ruddy Melgar y Sra",
         passes: 2
     },
     "eF1mG6x": {
-        names: "CIRILO MELGAR",
+        names: "Cirilo Melgar",
         passes: 1
     },
     "yI8cH7z": {
-        names: "MARIELA AGUILAR",
+        names: "Mariela Aguilar",
         passes: 1
     },
     "rO5pQ8a": {
-        names: "NELDA OLIVA Y ESPOSO",
+        names: "Nelda Oliva y Esposo",
         passes: 2
     },
     "gP2sT9b": {
-        names: "PERCY AREVALO Y ESPOSA",
+        names: "Percy Arevalo y Esposa",
         passes: 2
     },
     "lK9dU1c": {
-        names: "OSCAR RIBERA Y NOVIA",
+        names: "Oscar Ribera y Novia",
         passes: 2
     },
     "oJ6fV2d": {
-        names: "DAVID GALVEZ Y ESPOSA",
+        names: "David Galvez y Esposa",
         passes: 2
     },
     "iH3gW3e": {
-        names: "DAVID BANEGAS",
+        names: "David Banegas",
         passes: 1
     },
     "nL7hX4f": {
-        names: "FRANKLIN SAUCEDO Y ESPOSA",
+        names: "Franklin Saucedo y Esposa",
         passes: 2
     },
     "mC4iY5g": {
-        names: "RODRIGO BLUMBERG",
+        names: "Rodrigo Blumberg",
         passes: 2
     },
     "vB1jZ6h": {
-        names: "RONY ESCALANTE Y ESPOSA",
+        names: "Rony Escalante y Esposa",
         passes: 2
     },
     "cR8k A7i": {
-        names: "HUGO RIBERA Y NOVIA",
+        names: "Hugo Ribera y Novia",
         passes: 2
     },
     "zS5lB8j": {
-        names: "PEDRO VARGAS Y ESPOSA",
+        names: "Pedro Vargas y Esposa",
         passes: 2
     },
     "dX2mC9k": {
-        names: "SERGIO LOPEZ Y ESPOSA",
+        names: "Sergio Lopez y Esposa",
         passes: 2
     },
     "pY9nD1l": {
-        names: "OSCAR RUIZ Y ESPOSA",
+        names: "Oscar Ruiz y Esposa",
         passes: 2
     },
     "jA6oE2m": {
-        names: "CARLOS MAIDA",
+        names: "Carlos Maida",
         passes: 1
     },
     "bF3pG3n": {
-        names: "ISRAEL SAUCEDO",
+        names: "Israel Saucedo",
         passes: 1
     },
     "kH7qH4o": {
-        names: "FERNANDO CRESPO",
+        names: "Fernando Crespo",
         passes: 1
     },
     "uI4rI5p": {
-        names: "FERNANDO ENCINAS Y ESPOSA",
+        names: "Fernando Encinas y Esposa",
         passes: 2
     },
     "eM1sJ6q": {
-        names: "WAGNER INTURIAS",
+        names: "Wagner Inturias",
         passes: 1
     },
     "sN8tK7r": {
-        names: "ALEJANDRO ENCINAS Y NOVIA",
+        names: "Alejandro Encinas y Novia",
         passes: 2
     },
     "fO5uL8s": {
-        names: "MARCELO CORTEZ",
+        names: "Marcelo Cortez",
         passes: 1
     },
     "tP2vM9t": {
-        names: "CESAR CHOQUE",
+        names: "Cesar Choque",
         passes: 1
     },
     "gQ9wN1u": {
-        names: "RODRIGO AVILA",
+        names: "Rodrigo Avila",
         passes: 1
     },
     "lR6xO2v": {
-        names: "EDWIN PAZ SOLDAN Y ESPOSA",
+        names: "Edwin Paz Soldan y Esposa",
         passes: 2
     },
     "oT3yP3w": {
-        names: "MARCELO PAZ SOLDAN Y ESPOSA",
+        names: "Marcelo Paz Soldan y Esposa",
         passes: 4
     },
     "iU7zQ4x": {
-        names: "MANUEL VILLANOVA Y SRA",
+        names: "Manuel Villanova y Sra",
         passes: 2
     },
     "nJ4aR5y": {
-        names: "RUDDY PAZ SOLDAN",
+        names: "Ruddy Paz Soldan",
         passes: 1
     },
     "mV8bS6z": {
-        names: "CARLOS PAZ SOLDAN Y ESPOSA",
+        names: "Carlos Paz Soldan y Esposa",
         passes: 2
     },
     "cW5cT7a": {
-        names: "LORENA PAZ SOLDAN",
+        names: "Lorena Paz Soldan",
         passes: 1
     },
     "zX2dU8b": {
-        names: "IRIS PAZ SOLDAN Y NOVIO",
+        names: "Iris Paz Soldan y Novio",
         passes: 2
     },
     "dY9eV9c": {
-        names: "MARLENE PAZ SOLDAN",
+        names: "Marlene Paz Soldan",
         passes: 1
     },
     "pA6fW1d": {
-        names: "GABRIELA PAZ SOLDAN Y NOVIO",
+        names: "Gabriela Paz Soldan y Novio",
         passes: 2
     },
     "jB3gX2e": {
-        names: "ARACELY SORIA Y NOVIO",
+        names: "Aracely Soria y Novio",
         passes: 2
     },
     "kC7hY3f": {
-        names: "RENE FLORES",
+        names: "Rene Flores",
         passes: 2
     },
     "uD4iZ4g": {
-        names: "LEANDRO FLORES",
+        names: "Leandro Flores",
         passes: 2
     },
     "eE1jA5h": {
-        names: "BASILIA SALAZAR",
+        names: "Basilia Salazar",
         passes: 1
     },
     "sF8kB6i": {
-        names: "FELICIDAD FLORES",
+        names: "Felicidad Flores",
         passes: 1
     },
     "fG5lC7j": {
-        names: "ERNESTO FLORES",
+        names: "Ernesto Flores",
         passes: 2
     },
     "tH2mD8k": {
-        names: "JOSE FELICIANO FLORES Y SRA",
+        names: "Jose Feliciano flores y Sra",
         passes: 2
     },
     "gI9nE9l": {
-        names: "MIRTHA",
+        names: "Mirtha",
         passes: 1
     },
     "lJ6oF1m": {
-        names: "OTTO PICTOR",
+        names: "Otto Pictor",
         passes: 1
     },
     "oK3pG2n": {
-        names: "NICOLE VIRUEZ Y NOVIO",
+        names: "Nicole Viruez y Novio",
         passes: 2
     },
     "iL7qH3o": {
-        names: "SAUL PARADA Y ESPOSA",
+        names: "Saul Parada y Esposa",
         passes: 2
     },
     "nN4rI4p": {
-        names: "ANDREA SORICH Y NOVIO",
+        names: "Andrea Sorich y Novio",
         passes: 2
     },
     "mM1sJ5q": {
-        names: "DIANA MELGAR Y ESPOSO",
+        names: "Diana Melgar y Esposo",
         passes: 2
     },
     "cO8tK6r": {
-        names: "ASHLLY SALAZAR Y ESPOSO",
+        names: "Ashlly Salazar y Esposo",
         passes: 2
     },
     "zP5uL7s": {
-        names: "LAURA BARRIENTOS",
+        names: "Laura Barrientos",
         passes: 1
     },
     "dQ2vM8t": {
-        names: "XIMENA BURGOS",
+        names: "Ximena Burgos",
         passes: 1
     },
     "pE9wN9u": {
-        names: "CINTHIA IRIARTE Y NOVIO",
+        names: "Cinthia Iriarte y Novio",
         passes: 2
     },
     "jF6xO1v": {
-        names: "ALEJANDRA ARAMAYO",
+        names: "Alejandra Aramayo",
         passes: 1
     },
     "kG3yP2w": {
-        names: "MARIEL CABALLERO",
+        names: "Mariel Caballero",
         passes: 1
     },
     "uH7zQ3x": {
-        names: "CLAUDIA PARRA",
+        names: "Claudia Parra",
         passes: 1
     },
     "eI4aR4y": {
-        names: "ANDREA BALCAZAR Y ANDRES AG. AMIGOS",
+        names: "Andrea Balcazar y Andres",
         passes: 2
     },
     "sJ1bS5z": {
-        names: "DIEGO PARRA Y ESPOSA",
+        names: "Diego Parra y Esposa",
         passes: 2
     },
     "fK8cT6a": {
-        names: "LAURA CABALLERO Y NOVIO",
+        names: "Laura Caballero y Novio",
         passes: 2
     },
     "tL5dU7b": {
-        names: "MAITHE TUERO Y RODRIGO",
+        names: "Maithe Tuero y Rodrigo",
+        passes: 2
+    },
+    "xY2rP8c": {
+        names: "Anita Leon e Hijo",
+        passes: 2
+    },
+    "mZ9sQ4d": {
+        names: "Maria Luz paz Soldan y Esposo",
+        passes: 2
+    },
+    "aB6tW1e": {
+        names: "Margoth Paz Soldan",
+        passes: 1
+    },
+    "nC3uX7f": {
+        names: "America Cespedes",
+        passes: 2
+    },
+    "vD7vY8g": {
+        names: "Mirtha Alcazar",
+        passes: 1
+    },
+    "hE4wZ5h": {
+        names: "Aracely Soria",
         passes: 2
     }
 };
